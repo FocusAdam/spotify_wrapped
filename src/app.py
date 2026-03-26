edc9e8c7dc7c914b325bc9c8ea7898824951e0e0
Adam Tarkowski <adamtarkowski00@gmail.com>
2026-03-26 23:22:10 +0100

    feat: SQLAlchemy implementation

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import os
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import psycopg2
 
@@ -18,13 +19,16 @@ st.markdown("""
 load_dotenv()
 
 @st.cache_resource
-def get_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        database=os.getenv("POSTGRES_DB", "spotify_db"),
-        user=os.getenv("POSTGRES_USER", "postgres"),
+def get_engine():
+        host=os.getenv("DB_HOST", "192.168.8.130")
+        database=os.getenv("POSTGRES_DB", "spotify_db")
+        user=os.getenv("POSTGRES_USER", "postgres")
         password=os.getenv("POSTGRES_PASSWORD", "postgres")
-    )
+        port = os.getenv("DB_PORT", "5432")
+
+        db_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+        return create_engine(db_url, pool_size=5, max_overflow=10)
+    
 
 with st.sidebar:
     st.image("https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg", width=60)
@@ -52,7 +56,7 @@ with st.container():
 st.divider()
 
 try:
-    conn = get_connection()
+    engine = get_engine()
 
     query = f"""
         SELECT artist_name, COUNT(*) as play_count, SUM(ms_played) / 60000 as total_minutes
@@ -60,7 +64,7 @@ try:
         GROUP BY artist_name 
         ORDER BY total_minutes DESC LIMIT {limit_artystow}
     """
-    df = pd.read_sql_query(query, conn)
+    df = pd.read_sql_query(query, engine)
     
 
     if not df.empty:
@@ -90,3 +94,4 @@ try:
 
 except Exception as e:
     st.error(f"Nie udało się połączyć z bazą danych: {e}")
+    
